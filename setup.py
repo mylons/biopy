9294dea526007f4bdef5e2eab6b54d208ff28552
mylons <mike.lyons@courtagen.com>
2013-02-22 10:10:56 -0500

    setup works now

--- setup.py
+++ setup.py
@@ -1,1 +1,9 @@
 __author__ = 'mikelyons'
+from distutils.core import setup
+
+setup( name='biopy',
+       version="0.0.0.1",
+       description='various libs for bio',
+       author='Mike Lyons @mylons',
+       packages=['biopy']
+)
