d2ba26477465d3dab728f7fe4ff889c62f50d1d7
mylons <mike.lyons@courtagen.com>
2013-02-28 11:43:13 -0500

    classiifed and parameterized findLowCoverage.py

--- biopy/bedutil/findLowCoverage.py
+++ biopy/bedutil/findLowCoverage.py
@@ -1,11 +1,28 @@
 import sys
 from biopy.bedutil.io import *
 
-reader = BedReader("/tmp/coverage.bed", BedBuilder.coverage_depth_bed)
+class FindLowCoverage(object):
+    def __init__(self, bed_file, depth_max=10, bed_type=BedBuilder.coverage_depth_bed):
+        self._bed_file = bed_file
+        self.depth_max = depth_max
+        self._bed_type = bed_type
 
-depth_limit = 10
+    def parse_beds(self):
+        reader = BedReader(self._bed_file, self._bed_type)
+        for bed in reader.beds():
+            if bed.depth_at_position() < self.depth_max:
+                print str(bed)
 
-for bed in reader.beds():
-    if bed.depth_at_position() < 10:
-        print str(bed)
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description='find low coverage positions in coverageBed -d bed file')
+    parser.add_argument('-b', '--bed', help='bed file', required=True)
+    parser.add_argument('-t', '--type', help='bed file types supported: [coverage_depth_bed]', )
+    args = vars(parser.parse_args())
+
+    bed_type = (args['type'] if args['type'] else 'coverage_depth_bed')
+
+    f = FindLowCoverage(args['bed'])
+    f.parse_beds()
+
 
