a184921ccc0de818e04aad0dbc28d59ebf89e2b8
mylons <mike.lyons@courtagen.com>
2013-04-03 15:43:35 -0400

    better handling of info field

--- biopy/vcfutil/vcf.py
+++ biopy/vcfutil/vcf.py
@@ -4,13 +4,23 @@ class VCF(object):
 
     def __init__(self, vcfLine):
         self._tokens = vcfLine.split()
-        self._chrom = tokens[0]
-        self._pos = int(tokens[1])
-        self._id = tokens[2]
-        self._ref = tokens[3]
-        self._alt = tokens[4]
-        self._qual = float(tokens[5])
-        self._filter = tokens[6]
+        self._chrom = self._tokens[0]
+        self._pos = int(self._tokens[1])
+        self._id = self._tokens[2]
+        self._ref = self._tokens[3]
+        self._alt = self._tokens[4]
+        self._qual = float(self._tokens[5])
+        self._filter = self._tokens[6]
+        #make info
+        self._info = {}
+        if len(self._tokens) > 6:
+            info_tokens = self._tokens.split(";")
+            for toke in info_tokens:
+                tokes = toke.split("=")
+                if len(tokes) > 1:
+                    self._info[tokes[0]] = tokes[1]
+                else:
+                    self._info[tokes[0]] = True
 
     def chrom(self):
         return self._chrom
@@ -32,6 +42,15 @@ class VCF(object):
 
     def filter(self):
         return self._filter
+
+    def has_info_key(self, key):
+        return key in self._info
+
+    def get_info_field(self, key):
+        if key in self._info:
+            return self._info[key]
+        else:
+            return None
 
     def __str__(self):
         return "\t".join(self._tokens)
